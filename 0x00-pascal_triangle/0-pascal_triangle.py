c809f4fa3721236d062d84f866b36040732dfee4
Umoh Andem <andemumoh@gmail.com>
2023-04-19 16:12:14 +0100

    Task 0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Pascal Triangle Interview Challenge"""
+
+
+def pascal_triangle(turns):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if turns <= 0:
+        return []
+
+    pascal_triangle = [0] * turns
+
+    for outerLoop in range(turns):
+        # define the first row and fill first and last idx with 1
+        nextLine = [0] * (outerLoop+1)
+        nextLine[0] = 1
+        nextLine[len(nextLine) - 1] = 1
+
+        for innerLoop in range(1, outerLoop):
+            if innerLoop > 0 and innerLoop < len(nextLine):
+                a = pascal_triangle[outerLoop - 1][innerLoop]
+                b = pascal_triangle[outerLoop - 1][innerLoop - 1]
+                nextLine[innerLoop] = a + b
+
+        pascal_triangle[outerLoop] = nextLine
+
+    return pascal_triangle
