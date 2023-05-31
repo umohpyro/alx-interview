41af7005555a849db67bab803e8ee2d278d94c1a
Umoh Andem <andemumoh@gmail.com>
2023-05-31 05:41:30 +0000

    fixed semistandard rules

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,19 +7,19 @@ const endpoint = process.argv[2];
 const api = 'https://swapi-api.alx-tools.com/api/';
 const url = api + 'films/' + endpoint + '/';
 request.get({ url: url }, function (error, response, body) {
-	if (!error) {
-		const characters = JSON.parse(body).characters;
-		sort(characters);
-	}
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    sort(characters);
+  }
 });
 
-function sort(characters) {
-	if (characters.length > 0) {
-		request.get({ url: characters.shift() }, function (err, res, body) {
-			if (!err) {
-				console.log(JSON.parse(body).name);
-				sort(characters);
-			}
-		});
-	}
+function sort (characters) {
+  if (characters.length > 0) {
+    request.get({ url: characters.shift() }, function (err, res, body) {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        sort(characters);
+      }
+    });
+  }
 }
